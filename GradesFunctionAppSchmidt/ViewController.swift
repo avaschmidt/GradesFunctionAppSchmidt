cb3355ba053aec27a4627e88f2c93cbb3ada9534
Ava Schmidt <avamschmidt07@gmail.com>
2024-09-19 08:02:40 -0500

    MVP and Stretch 1 Complete

--- GradesFunctionAppSchmidt/ViewController.swift
+++ GradesFunctionAppSchmidt/ViewController.swift
@@ -1,19 +1,77 @@
-//
-//  ViewController.swift
-//  GradesFunctionAppSchmidt
-//
-//  Created by AVA SCHMIDT on 9/18/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayOutlet: UILabel!
+    @IBOutlet weak var possibleOutlet: UITextField!
+    @IBOutlet weak var earnedOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func submitAction(_ sender: UIButton) {
+        if possibleOutlet.text != nil && earnedOutlet.text != nil {
+            
+            if let check1 = Double(possibleOutlet.text!){
+                if let check2 = Double(earnedOutlet.text!){
+                    var pos = check1
+                    var ear = check2
+                    displayOutlet.text = "\(passOrFail(pointsEarned: ear, pointsPossible: pos)) ~ Grade: \(letterGrade(percentage: ear/pos))"
+                }
+            }
+            else{
+                displayOutlet.text = errorMessage()
+            }
+            
+        }
+        else{
+            displayOutlet.text = errorMessage()
+        }
+        
+    }
+    
+ 
+    func passOrFail(pointsEarned pe: Double, pointsPossible pp: Double)-> String{
+        
+        var score = pe/pp
+        if  score >= 0.60{
+            return "You Passed!"
+        }
+        else{
+            return "You Failed!"
+        }
+      
+    }
+    
+    func errorMessage()->String{
+        var err = "Error"
+        return err
+    }
+    
+    func letterGrade(percentage p: Double)->String{
+        if p < 0.6{
+            return "F"
+        }
+        else if p < 0.7{
+            return "D"
+        }
+        else if p < 0.8{
+            return "C"
+        }
+        else if p < 0.9{
+            return "B"
+        }
+        else if p < 1.0 || p == 1.0{
+            return "A"
+        }
+        else{
+            return errorMessage()
+        }
+        
+    }
+    
+    
 
 }
 
